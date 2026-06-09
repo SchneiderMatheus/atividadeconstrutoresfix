611fffc5ea8492cce75b7e055613d2c8a8ed4cbb
Matheus <matheuswschneider@gmail.com>
2026-06-09 16:31:04 -0300

    Trying and trying

--- Program/src/application/App.java
+++ Program/src/application/App.java
@@ -1,6 +1,50 @@
 package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Cadastro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Cadastro cadastro = new Cadastro(0, null, 0);
+
+        System.out.print("Enter account number: ");
+        int accNumber = sc.nextInt();
+
+        System.out.print("Enter account holder: ");
+        String name = sc.nextLine();
+
+        System.out.println("Is there an initial deposit y/n)?");
+        String yesOrNo = sc.nextLine();
+        if (yesOrNo.equals("y")) {
+            System.out.println("Enter the intial deposit value: ");
+            double balance = sc.nextDouble();
+        } else {
+            double balance = 0;
+        }
+
+        Cadastro.print(accNumber, name, accNumber);
+
+        System.out.print("\nEnter a deposite value: ");
+        cadastro.addBalance(sc.nextDouble());
+
+        Cadastro.print(accNumber, name, accNumber);
+
+        System.out.print("enter a withdraw value:");
+        double withdraw = sc.nextDouble();
+        while (withdraw > cadastro.getBalance() -5) {
+                System.out.println("THATS MORE THAN WHAT YOU HAVE, REMEMBER IS COSTS YOU $5 TO MAKE A WITHDRAW! TRY AGAIN");
+                System.out.print("enter a withdraw value:");
+                withdraw = sc.nextDouble();
+        }
+
+        cadastro.removeBalance(withdraw);
+        System.out.println();
+        Cadastro.print(accNumber, name, accNumber);
+
+        sc.close();
     }
 }
--- Program/src/entities/Cadastro.java
+++ Program/src/entities/Cadastro.java
@@ -1,7 +1,42 @@
 package entities;
 
 public class Cadastro {
-    private int accNumber;
+    private final int accNumber;
     private String name;
     private double balance;
+
+    public Cadastro(int accNumber, String name, double balance){
+        this.accNumber = accNumber;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAcc(){
+        return accNumber;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    public double getBalance(){
+        return balance;
+    }
+
+    public void addBalance(double balance){
+        this.balance += balance;
+    }
+    public void removeBalance (double balance){
+        this.balance -=balance -5;
+    }
+
+    public static void print(int accNumber, String name, double balance) {
+        System.out.println("Account data: ");
+        System.out.print("Account: "+accNumber+"Holder: "+name + "Balance: $ "+balance); 
+    
+    }
 }
